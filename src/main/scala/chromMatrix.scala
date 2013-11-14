f895af69aee4ca3c1c286e64328b81b22674bd36
mirin <panda.1192@gmail.com>
2013-11-14 23:09:56 +0900

    initial commit

---
+++ src/main/scala/chromMatrix.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yotaro
+ * Date: 2013/11/14
+ * Time: 18:10
+ * To change this template use File | Settings | File Templates.
+ */
+class chromMatrix {
+
+}
